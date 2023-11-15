a1cad3ce48266f08b673820ae10cd7ef16a63b04
pavjav <pavel.javornik00@gmail.com>
2023-11-15 17:08:08 -0500

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "15%";
+document.getElementById("myClsdSideNav").style.width = "0";
+    
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("myClsdSideNav").style.width = "3%";
+
 }
